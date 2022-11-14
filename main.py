cb9855d4d3d43d7a5dd2482eabd2eefdab3462a9
SirLandr <79620369+SirLandr@users.noreply.github.com>
2022-11-13 23:29:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+class Vehicel:  
+        Type = input("Insert the type of your vehicel you own: ")
+        Make = input("Insert the make of your vehicel: ")
+        Model = input ("Insert the model of your vehicel: ")
+        Year = input('Input the year of your vehicel: ')
+        Numberofdoors = input("Input the number of doors your vehicel has: ")
+        Roof = input("Input the type of roof your vehicel has:")
+
+
+        print("Your car type is:", Type)
+        print("You car make is:" ,Make)
+        print("Your cars model is:" ,Model)
+        print("Your cars year is:", Year)
+        print("Your cars has:" ,Numberofdoors ,"number of doors")
+        print("Your car has a:" ,Roof, "roof")
